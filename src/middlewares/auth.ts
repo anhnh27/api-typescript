4fe154ff8d84d1ac5f04f182523bc6a80a54485e
alex.nguyen <nguyenhoanganh10290@gmail.com>
2021-07-24 20:42:54 +0800

    - u

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 
 export const authorization = (req: Request, res: Response, next: any) => {
+    console.log('SESSION: ', req.session)
     if (req.session && req.session.cookie)
         return next();
     else
